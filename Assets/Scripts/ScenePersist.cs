812d02c1ca91f3382edbd35b99aca9fc93a6ca33
insan <insan.putranda@gmail.com>
2021-07-22 00:08:29 +0700

    Set initial logic for persist pickups

---
+++ Assets/Scripts/ScenePersist.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenePersist : MonoBehaviour
+{
+    public int startingSceneIndex;
+    private void Awake()
+    {
+        if (FindObjectsOfType<ScenePersist>().Length > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingSceneIndex = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex != startingSceneIndex) Destroy(gameObject);
+    }
+}
